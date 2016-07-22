683282acb0346cd5b5d60535b82797b7ad11ef36
LukeInCloud <github@lukeincloud.com>
2016-07-22 21:18:29 +0200

    Added package directory

---
+++ frontend_logger/__init__.py
@@ -0,0 +1,16 @@
+"""
+Django Frontend Logger
+by www.LukeinCloud.com
+Sources:
+https://github.com/LukeInCloud/django-frontend-logger
+Tutorial:
+https://www.lukeincloud.com/tag/django-frontend-logger/
+"""
+
+__title__ = 'Django Frontend Logger'
+__version__ = '0.0.1'
+__author__ = 'Lucas Jarzembowski'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2016 LukeInCloud.com'
+
+from logger import logger
