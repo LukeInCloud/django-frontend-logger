2e0227c6ad712384e7fbd91ddbac998df5868e26
LukeInCloud <LukeInCloud@users.noreply.github.com>
2016-08-22 23:40:48 +0200

    Further simplification

--- frontend_logger/utils.py
+++ frontend_logger/utils.py
@@ -18,14 +18,8 @@ class FuncWrapper(object):
     def __init__(self, func, arg_num=0, *f_args, **f_kwargs):
         self.func = func
         self.arg_num = arg_num
-        if f_args:
-            self.f_args = f_args
-        else:
-            self.f_args = []
-        if f_kwargs:
-            self.f_kwargs = f_kwargs
-        else:
-            self.f_kwargs = {}
+        self.f_args = f_args
+        self.f_kwargs = f_kwargs
 
     def __call__(self, o):
         args = list(self.f_args)
